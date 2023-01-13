b21e6e351477f6fb04f3c8c3b89ddea4643c5f28
Gazelle2010 <martin-schwerdtner@t-online.de>
2023-01-13 14:31:35 +0100

    02 Deklaration Update

--- script_01.js
+++ script_01.js
@@ -14,10 +14,23 @@
 
 /***** 02 Deklaration + Wertzuweisung I *******/
 
-let firstName; // Deklaration (Definition)
-firstName = "Yunaisy"; // Wertzuweisung
-console.log(firstName); //Ausgabe
+// let firstName; // Deklaration (Definition)
+// firstName = "Yunaisy"; // Wertzuweisung
+// console.log(firstName); //Ausgabe
 
-let familyName = "Jug";
-console.log(familyName);
-console.log(firstName + " " + familyName);
+// let familyName = "Jug";
+// console.log(familyName);
+// console.log(firstName + " " + familyName);
+
+/* JS ist eine untypisierte Sprache! | untyped */
+
+let test;
+test = "Hi";
+test = 2;
+test = true
+// console.log(test);
+
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test); // Ausgabe
+
+
